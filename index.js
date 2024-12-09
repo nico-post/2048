932b44663c722b0e8d24d8f31eb7ee08d9cf039b
Nico <nicholas.post28@student.tamdistrict.org>
2024-12-09 14:30:09 -0800

    Co-authored-by: adamscheer4 <adamscheer4@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,22 @@
+let grid = [
+    [0,1,2,3]
+    [4,5,6,7]
+    [8,9,10,11]
+    [12,13,14,15]
+]
+let cell00 = 0
+let cell01 = 0
+let cell02 = 0
+let cell03 = 0
+let cell10 = 0
+let cell11 = 0
+let cell12 = 0
+let cell13 = 0
+let cell20 = 0]
+let cell21 = 0
+let cell22 = 0
+let cell23 = 0
+let cell30 = 0
+let cell31 = 0
+let cell32 = 0
+let cell33 = 0
